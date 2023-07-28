dba1f4227b7a0268a864794c1bc9301d9defff37
Shivaprasadumshette <93588167+Shivaprasadumshette@users.noreply.github.com>
2023-07-28 15:49:33 +0530

    hell1

--- command.sh
+++ command.sh
@@ -2,7 +2,7 @@ sudo apt -y update
 sudo apt -y upgrade
 sudo apt-get install libopenmpi-dev
 mpicc -o mpi_sum mpi_sum.c
-mpiexec -n <number_of_processes> ./mpi_sum
+mpiexec -n 4 ./mpi_sum
 
 gcc -o hello_openmp hello_openmp.c -fopenmp
 ./hello_openmp

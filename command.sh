c04c812841d694913492148bf2e5c63918cdca69
Shivaprasadumshette <93588167+Shivaprasadumshette@users.noreply.github.com>
2023-07-28 16:20:04 +0530

    now

--- command.sh
+++ command.sh
@@ -1,8 +1,15 @@
 sudo apt -y update
 sudo apt -y upgrade
 sudo apt-get install libopenmpi-dev
-mpicc -o mpi_sum mpi_sum.c
-mpiexec -n 4 ./mpi_sum
+
+
+mpicc mpi_sum.c 
+mpiexec -n 4 ./a.out 
 
 gcc -o hello_openmp hello_openmp.c -fopenmp
 ./hello_openmp
+
+
+
+gcc -fopenmp VecAdd.c
+./a.out
